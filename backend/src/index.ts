5bd1bb5f0aa020304effc71ea8536c6430648768
t-abe09 <t.abe@wakuto.net>
2024-02-19 15:11:52 +0900

    🚀  deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,4 @@
-// import { Server } from "./lib/app";
+import { Server } from "./lib/app";
 
-// const server = new Server();
-// server.Run();
-
-import { fetchStockData } from "./lib/service/fetchYahooFinance";
-
-async function main() {
-  const stocks = ["AAPL", "GOOG", "TSLA", "MSFT"];
-
-  const data = await fetchStockData(stocks);
-
-  // 取得結果の表示
-  data.map((stock) => {
-    console.log(stock);
-  });
-}
-
-main();
+const server = new Server();
+server.Run();
